699b2a1452921cfc3509fa0b6fadb0e39f2bc974
Enes Özdemir <ozdemirenes558@gmail.com>
2024-11-17 03:59:35 +0300

    first commit

---
+++ backend/src/main/java/com/project/reshmotion/ReshmotionApplication.java
@@ -0,0 +1,13 @@
+package com.project.reshmotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshmotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReshmotionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/reshmotion/ReshmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reshmotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReshmotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

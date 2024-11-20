dbe0a0ca57f358621eb8c098becd992ffa17a54a
Enes Özdemir <ozdemirenes558@gmail.com>
2024-11-20 04:33:33 +0300

    mobile responsive design completed for navbar #15

---
+++ frontend/src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="container mx-auto py-20 px-5 sm:px-0 min-h-screen">
+      AboutPage
+    </div>
+  );
+}
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="container mx-auto py-20 px-5 sm:px-0 min-h-screen">
+      Home
+    </div>
+  )
+}

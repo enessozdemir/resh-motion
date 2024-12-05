7d277948f465d371cd2107af5bffe6448b18d0c5
Enes Özdemir <ozdemirenes558@gmail.com>
2024-12-05 18:04:14 +0300

    about page completed #38

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,7 +1,35 @@
 export default function AboutPage() {
   return (
-    <div className="container mx-auto py-20 px-5 sm:px-0 min-h-screen">
-      AboutPage
+    <div className="text-alt-black container mx-auto pt-32 px-5 sm:px-0 min-h-screen">
+      <div className="w-1/2 mx-auto">
+        <div className="sm:text-6xl font-bold flex flex-col items-center gap-1">
+          <p className="text-silver">Yıllardır Süregelen</p>
+          <p>Tecrübe</p>
+        </div>
+
+        <div className="opacity-80 leading-7 flex flex-col gap-5 text-xl items-center mt-16">
+          <p>
+            Resh Motion olarak, araç kiralama deneyimini modern, hızlı ve
+            güvenli bir platformda yeniden tanımlamak için buradayız. Günümüzün
+            dinamik dünyasında, araç kiralama sürecinin karmaşık ve zaman alıcı
+            olmasını istemiyoruz. Bu yüzden, kullanıcı dostu bir tasarımla
+            ihtiyaçlarınıza hızlı ve kolay çözümler sunuyoruz.
+          </p>
+          <p>
+            Platformumuz, yenilikçi teknolojilerle desteklenerek her aşamada
+            güvenliğinizi ve memnuniyetinizi ön planda tutar. Hem bireysel hem
+            de kurumsal müşteriler için tasarlanan esnek çözümlerimizle, araç
+            kiralama deneyiminizi zahmetsiz hale getiriyoruz.
+          </p>
+          <p>
+            Resh Motion, araç kiralamayı yalnızca bir hizmet değil, bir deneyim
+            haline getirmek için çalışıyor. Modern altyapımız, size her an
+            ulaşabileceğiniz kesintisiz bir hizmet sunarken, hızlı rezervasyon
+            sistemimiz sayesinde ihtiyaçlarınıza en uygun aracı bulmanız sadece
+            birkaç tıklama uzaklıkta.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

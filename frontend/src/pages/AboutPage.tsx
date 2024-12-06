45d2addfc32a42262e880d55da560c2dbe50d77e
Enes Özdemir <ozdemirenes558@gmail.com>
2024-12-06 18:27:18 +0300

    about page responsive design made

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,13 +1,13 @@
 export default function AboutPage() {
   return (
-    <div className="text-alt-black container mx-auto pt-32 px-5 sm:px-0 min-h-screen">
-      <div className="w-1/2 mx-auto">
-        <div className="sm:text-6xl font-bold flex flex-col items-center gap-1">
+    <div className="text-alt-black container mx-auto sm:pt-32 pt-20 px-5 sm:px-0 min-h-screen">
+      <div className="sm:w-1/2 w-full mx-auto">
+        <div className="sm:text-6xl text-3xl font-bold flex flex-col items-center gap-1">
           <p className="text-silver">Yıllardır Süregelen</p>
           <p>Tecrübe</p>
         </div>
 
-        <div className="opacity-80 leading-7 flex flex-col gap-5 text-xl items-center mt-16">
+        <div className="opacity-80 leading-7 flex flex-col gap-5 text-xl items-center sm:mt-16 mt-10 sm:pb-0 pb-10">
           <p>
             Resh Motion olarak, araç kiralama deneyimini modern, hızlı ve
             güvenli bir platformda yeniden tanımlamak için buradayız. Günümüzün
